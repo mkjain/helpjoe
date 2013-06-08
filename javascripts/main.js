8f160cd83fb298c56e11038eb0231c3ed2ef8f87
farhad <farhad@earthbook>
2013-06-09 00:28:09 +0200

    added bootstrap jinc

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,8 @@ var kiezDAO;
 
 $(document).ready (function () {
     console.log ("running the startup script");
-    
-    kiezDAO = new Firebase('https://4kiez.firebaseio.com/zips/10932');
-    
-    kiezDAO.push ({item : {name : "test",votes : 0}});
 });
 
-function addItem (zip,name,description,votes) {
-        
-}
+$("#GoAction").click (function () {
+    window.location = "http://www.4kiez.de/projects.html?zip=" + $("#inputZIP").val ();    
+});
