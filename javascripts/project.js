5775b3d88e22d880f2295757401bf67629924a9a
farhad <farhad@earthbook>
2013-06-09 00:45:55 +0200

    added bootstrap jinc

--- javascripts/project.js
+++ javascripts/project.js
@@ -37,5 +37,6 @@ function addItem (zip,name,description,votes) {
 
 function renderProjects(data) {
     var code = '<div class=\'row-fluid\' id=\'main_content_wrap\'><div class=\'span3\'></div><div class=\'span6\'><div class=\'row\'><div class=\'span8\'> <h4><strong><a href=\'#\'>' + data.name + '</a></strong></h4>\n</div>\n</div>\n<div class=\'row\'>\n<div class=\'span2\'>\n<a href=\'#\' class=\'thumbnail\'>\n<img src=\'http://placehold.it/260x180\' alt=\'\'>\n</a>\n</div>\n<div class=\'span6\'>\n<p>' + data.description + '</p>\n<p><a class=\'btn\' href=\'#\'>Read more</a></p>\n</div>\n</div>\n<div class=\'row\'>\n<div class=\'span8\'>\n<p></p>\n<p>\n<i class=\'icon-user\'></i> by <a href=\'#\'>John</a>\n| <i class=\'icon-calendar\'></i> Sept 16th, 2012\n| <i class=\'icon-thumbs-up\'></i> <a href=\'#\'>' + data.vote + ' Votes</a>\n| <i class=\'icon-share\'></i> \n| <i class=\'icon-tags\'></i> Tags : <a href=\'#\'><span class=\'badge badge-info\'>Snipp</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>Bootstrap</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>UI</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>growth</span></a>\n</p>\n</div>\n</div>\n</div>\n<div class=\'span3\'></div>\n' + '</div>';
+    
     return code;
 }
