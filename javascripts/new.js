1ad702aac191215a8308498320a64ff9f52ae0bf
farhad <farhad@earthbook>
2013-06-09 00:25:49 +0200

    added bootstrap jinc

--- javascripts/new.js
+++ javascripts/new.js
@@ -3,13 +3,16 @@ var kiezDAO;
 $(document).ready (function () {
     console.log ("running the new script");
     
-    //fireurl = 'https://4kiez.firebaseio.com/zips/' + zipCode;
-    
-    //kiezDAO = new Firebase(fireurl);
-    
+     fireurl =  'https://4kiez.firebaseio.com/zips';
+     kiezDAO = new Firebase(fireurl);
+
     $("#createNewItem").click(function () {
         var zip = $("#inputZIP").val ();
         var name = $("#inputName").val ();
-        var description = $("#inputDescription").val ();
+        var description = $("#inputDescription").val ();      
+        
+        kiezDAO.child (zip).push ({name : name, description : description, vote : 0}); 
+        
+        window.location = "file:///home/farhad/NetBeansProjects/4kiez/helpjoe/projects.html?zip=" + zip;
     });
 });
--- javascripts/project.js
+++ javascripts/project.js
@@ -5,11 +5,18 @@ $(document).ready (function () {
     
     zipCode = getQueryVariable("zip");
     
-    $("#project_tagline").html (zipCode);
+    $("#project_tagline").html ("Projects for " + zipCode);
     
     fireurl = 'https://4kiez.firebaseio.com/zips/' + zipCode;
     
     kiezDAO = new Firebase(fireurl);
+    
+    kiezDAO.on ('child_added',function (snapshot) {
+        var msgData = snapshot.val();
+        
+        $("#project_list").append (renderProjects(msgData));
+    });
+    
 });
 
 function getQueryVariable(variable) {
@@ -28,6 +35,8 @@ function addItem (zip,name,description,votes) {
         
 }
 
-function renderProjects() {
-    
+function renderProjects(data) {
+    console.log (data);
+    var code = '<div class=\'row-fluid\' id=\'main_content_wrap\'><div class=\'span3\'></div><div class=\'span6\'><div class=\'row\'><div class=\'span8\'> <h4><strong><a href=\'#\'>' + data.name + '</a></strong></h4>\n</div>\n</div>\n<div class=\'row\'>\n<div class=\'span2\'>\n<a href=\'#\' class=\'thumbnail\'>\n<img src=\'http://placehold.it/260x180\' alt=\'\'>\n</a>\n</div>\n<div class=\'span6\'>\n<p>' + data.description + '</p>\n<p><a class=\'btn\' href=\'#\'>Read more</a></p>\n</div>\n</div>\n<div class=\'row\'>\n<div class=\'span8\'>\n<p></p>\n<p>\n<i class=\'icon-user\'></i> by <a href=\'#\'>John</a>\n| <i class=\'icon-calendar\'></i> Sept 16th, 2012\n| <i class=\'icon-thumbs-up\'></i> <a href=\'#\'>' + data.vote + ' Votes</a>\n| <i class=\'icon-share\'></i> \n| <i class=\'icon-tags\'></i> Tags : <a href=\'#\'><span class=\'badge badge-info\'>Snipp</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>Bootstrap</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>UI</span></a>\n<a href=\'#\'><span class=\'badge badge-info\'>growth</span></a>\n</p>\n</div>\n</div>\n</div>\n<div class=\'span3\'></div>\n</div>';
+    return code;
 }
